cd6186362e4ecaf0944485ab135b1b0563a62b27
Rachel Lim <lim-rachel@users.noreply.github.com>
2021-09-05 17:20:37 -0700

    fix 2.1 step1

--- 2.1/step1/UsingFields.java
+++ 2.1/step1/UsingFields.java
@@ -1,6 +1,5 @@
 class UsingFields {
-  int gravity = 10;
-  int distAfter2sec = (this.gravity / 2) * (2 * 2);
-  int distAfter4sec = (this.gravity / 2) * (4 * 4);
-  int distAfter6sec = (this.gravity / 2) * (6 * 6);
+  int distAfter2sec = (10 / 2) * (2 * 2);
+  int distAfter4sec = (10 / 2) * (4 * 4);
+  int distAfter6sec = (10 / 2) * (6 * 6);
 }
