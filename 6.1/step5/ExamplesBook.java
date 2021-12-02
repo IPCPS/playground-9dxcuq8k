61ff56ec99bddf449af7d4770f00caf420877b46
Rachel Lim <lim-rachel@users.noreply.github.com>
2021-12-01 22:25:52 -0800

    make intended autofold an actual autofold

--- 6.1/step5/ExamplesBook.java
+++ 6.1/step5/ExamplesBook.java
@@ -2,7 +2,7 @@ class Book {
     String title;
     String author;
     int price;
-  //autofold {
+  // { autofold
     Book(String title, String author, int price) {
       this.title = title;
       this.author = author;
